967b7e6dbea43e68ea2fb3f54bbb8ccef67a23c3
Kamal Nayan <95926324+legendarykamal@users.noreply.github.com>
2023-04-14 16:13:01 +0530

    Create store.js

---
+++ client/src/store/store.js
@@ -0,0 +1,8 @@
+import { RLNFullProof } from 'rlnjs'
+import BigInt from 'big-integer'
+
+// Getters & Setters for all RLNjs objects
+export const [epoch, setEpoch] = [1]; // Epoch
+export const [appID, setAppID] = [1234567890];
+// export const [publishQueue, setPublishQueue] = createSignal<PublishQueueType[]>([]) // Queue of proofs to be published
+// export const [publishedMsgProofs, setPublishedMsgProofs] = createSignal<PublishQueueType[]>([]) // List of published proofs
